2f7c9252ef0b988a5d8bd0eadd7ba04d22d5872d
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-09-04 23:04:57 +0800

    delete users default add auth controller for login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth as FacadesAuth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        return(back()->withErrors([
+            'username' => 'The provided credentials do not match our records.',
+        ]));
+    }
+}
